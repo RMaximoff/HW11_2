e41334f6fef486175ae6303d4283b337127fb379
RMaximoff <great.goodwin.92@gmail.com>
2023-02-18 17:18:11 +0300

    добавлены докстринги и функ capitalize

--- str_func.py
+++ str_func.py
@@ -1,3 +1,12 @@
 def str_upper():
+    """все заглавные"""
     user_input = str(input())
     return user_input.upper()
+
+def capitalize_words(string):
+    """загланые каждая первая"""
+    words = string.split()
+    sentence = []
+    for word in words:
+        sentence.append(word.capitalize())
+    return " ".join(sentence)
